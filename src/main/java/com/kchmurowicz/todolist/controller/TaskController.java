ef464d758d5936ff2211b202bfb8f30373248c96
kchmurowicz <k.chmurowicz@gmail.com>
2019-04-19 10:34:45 +0200

    added ability to get tasks

--- src/main/java/com/kchmurowicz/todolist/controller/TaskController.java
+++ src/main/java/com/kchmurowicz/todolist/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.kchmurowicz.todolist.service.TaskService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -29,5 +30,9 @@ public class TaskController {
         taskService.deleteTask(taskId);
     }
 
-
+    @GetMapping
+    public @ResponseBody
+    List<Task> getTasks() {
+        return taskService.findAll();
+    }
 }
--- src/main/java/com/kchmurowicz/todolist/service/TaskService.java
+++ src/main/java/com/kchmurowicz/todolist/service/TaskService.java
@@ -7,6 +7,7 @@ import com.kchmurowicz.todolist.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -40,5 +41,6 @@ public class TaskService {
         taskRepository.deleteById(taskId);
     }
 
+    public List<Task> findAll(){ return taskRepository.findAll();    }
 
 }
